c790c0438762575a79ab424becf102da9749298d
Leandro Ferreira <leandroferreira@MacBook-Pro-de-Leandro.local>
2019-10-23 00:12:56 -0300

    adicionado cadastro de usuario

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,16 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import StudentController from './app/controllers/StudentController';
 
 const routes = new Router();
 
-routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
+routes.post('/students', StudentController.store);
+routes.post('/users', UserController.store);
 routes.put('/users', UserController.update);
 
 export default routes;
