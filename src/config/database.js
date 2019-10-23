768abec12f213a7e36239ec3a9d6ce3f705b112c
Leandro Ferreira <leandroferreira@MacBook-Pro-de-Leandro.local>
2019-10-22 23:12:16 -0300

    finalizado parte de administrador

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  database: 'MeetApp',
+  database: 'gympoint',
   define: {
     timesstamps: true,
     underscored: true,
